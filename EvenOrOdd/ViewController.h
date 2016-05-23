c955bda1fd0d3eaf7db782b875945e8ac9c2b949
msmi1023 <msmi1023@ford.com>
2016-05-23 13:20:08 -0400

    Initial Commit

---
+++ EvenOrOdd/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EvenOrOdd
+//
+//  Created by tstone10 on 5/23/16.
+//  Copyright © 2016 DetroitLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ EvenOrOdd/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  EvenOrOdd
+//
+//  Created by tstone10 on 5/23/16.
+//  Copyright © 2016 DetroitLabs. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+@end
