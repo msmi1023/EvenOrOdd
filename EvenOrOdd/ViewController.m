745e5752a5f36fc487f877a888aa97e87b7446e6
msmi1023 <msmi1023@ford.com>
2016-05-23 14:09:24 -0400

    MVP of even or odd app

--- EvenOrOdd/ViewController.m
+++ EvenOrOdd/ViewController.m
@@ -10,6 +10,9 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet UILabel *resultLabel;
+
 @end
 
 @implementation ViewController
@@ -22,6 +25,36 @@
 - (void)didReceiveMemoryWarning {
 	[super didReceiveMemoryWarning];
 	// Dispose of any resources that can be recreated.
+}
+
+- (IBAction)evaluateInput:(id)sender {
+	//attempt grabbing input as a number
+	NSNumber *input = [self convertStringToNumber:[_textField text]];
+	
+	if(input != nil) {
+		//is numeric
+		//check for period to see if non-whole number
+		if  ([[_textField text] rangeOfString:@"."].location != NSNotFound) {
+			_resultLabel.text = @"Invalid Entry.";
+		}
+		else {
+			int inputInt = [input intValue];
+			if(inputInt % 2) {
+				_resultLabel.text = @"Odd Number";
+			}
+			else {
+				_resultLabel.text = @"Even Number";
+			}
+		}
+	}
+	else {
+		_resultLabel.text = @"Invalid Entry.";
+	}
+}
+
+- (NSNumber *)convertStringToNumber:(NSString *)inputText {
+	NSNumberFormatter *nf = [[NSNumberFormatter alloc] init];
+	return [nf numberFromString:inputText];
 }
 
 @end
